8e6eb0b90af23ea10b2d3954c58968b6e60361ec
lrpassos <luiz.rogerios@gmail.com>
2024-05-07 00:36:24 -0300

    Add files via upload

---
+++ Meu banco.py
@@ -0,0 +1,115 @@
+
+menu = """
+   ==================================  
+     Sejam Bem-vindos ao SEU BANCO
+  ====================================
+  
+    Escolha uma das opções abaixo:
+  
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[9] Sair
+
+=> """
+
+
+
+NovaTransacao = '''
+----------------------------------------
+      Deseja fazer outra transação
+----------------------------------------
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[9] Sair
+
+=>'''
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito:  "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+            print('''
+                  
+                  Operação realizada com Sucesso!!!! 
+                  
+                  
+                  ''')
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+            opcao = input(menu)
+
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        
+        
+    
+        
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente. \n")
+            print(" Tente novamente \n")
+            ##valor = float(input("Informe o valor do saque: "))
+            
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite. \n")
+            print(" Tente novamente \n")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido. \n")
+            print(" Tente novamente \n")
+            
+            
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            print('''
+                 
+                 
+                    Operação realizada com Sucesso!!!!     
+                  
+                 
+                  
+                  ''')
+            numero_saques += 1
+            
+            
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+        
+
+
+    elif opcao == "9":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        
+exit
